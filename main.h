333c42dc8a4b7b218f973ae46fa0a17832a6c58a
Theo <thoecastillo@yahoo.com>
2024-01-23 22:08:29 +0100

    init

---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by Theo Castillo on 22/01/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "SFML/Graphics.hpp"
+#include "iostream"
+
+using namespace std;
+
+const int WIN_WIDTH = 800;
+const int WIN_HEIGHT = 800;
+
+#endif //MAIN_H
